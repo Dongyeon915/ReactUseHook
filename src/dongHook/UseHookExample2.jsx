507521570bb362f75a4634c51bc0fbc5dff91b42
dongyeonkim <ehd9567@naver.com>
2023-02-14 18:37:23 +0900

    감소

--- src/dongHook/UseHookExample2.jsx
+++ src/dongHook/UseHookExample2.jsx
@@ -1,21 +1,26 @@
-import React,{useEffect,useReducer,useRef,useState} from "react";
+import React, {useReducer} from "react";
 
 export default function UseHookExample2() {
   // reducer - state를 업데이트 역활
   // action - type의 따라 state값을 변경시킨다
   // dispatch - action의 타입 전달
   const reducer = (state, action) => {
-    console.log("실행확인")
-    if (action.type === "증가"){
+    console.log("증가 확인")
+    if (action.type === "증가") {
       return {
         count: state.count + 1
+      }
+    } else if (action.type === "감소") {
+      console.log("감소 확인")
+      return {
+        count: state.count - 1
       }
     }
     return state
   }
 
   const initialState = {
-    count : 0
+    count: 0
   }
 
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -23,7 +28,8 @@ export default function UseHookExample2() {
   return (
       <div>
         <h3>{state.count}</h3>
-        <button onClick={(e) => dispatch({type:"증가"})}>증가버튼</button>
+        <button onClick={(e) => dispatch({type: "증가"})}>증가버튼</button>
+        <button onClick={(e) => dispatch({type: "감소"})}>감소버튼</button>
       </div>
   )
 }
